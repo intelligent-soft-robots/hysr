8b883f4116fd1dea09978791ead00a2073e08227
Vincent Berenz <vberenz@tuebingen.mpg.de>
2022-05-20 14:10:22 +0200

    unit testing for normalize and observations in progress

---
+++ tests/test_observations.py
@@ -0,0 +1,16 @@
+from hysr import hysr_types
+from hysr import observations
+
+
+def test_pack():
+
+    box = (0.,0.),(10.,10.)
+    
+    main_sim = hysr_types.MainSimState(
+        ()
+    
+    states = hysr_types.States()
+
+    
+
+    
