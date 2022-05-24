7eddb0c2bca465a620d89d9f00ff0db987a665b5
Vincent Berenz <vberenz@tuebingen.mpg.de>
2022-05-24 15:07:35 +0200

    progress on observation tests

--- tests/test_observations.py
+++ tests/test_observations.py
@@ -6,12 +6,12 @@ from hysr import observations
 def test_pack():
 
     box = (0.0, 0.0, 0.0), (10.0, 10.0, 10.0)
-    max_velocity = 1.0
+    max_velocity = 2.0
     max_angular_velocity = 1.0
     
     main_sim = hysr_types.MainSimState (
-        (5.,5.,5.),
-        (0.5,0.5,0.5),
+        (5.,7.5,5.),
+        (0.5,0.5,1.0),
         (0.,0.,0.,0.),
         (0.5,0.5,0.5,0.5),
         (
@@ -28,5 +28,39 @@ def test_pack():
         ("ball_position",),
         box, max_velocity, max_angular_velocity
     )
+    assert tuple(packed) == (0.5,0.75,0.5)
 
-    assert tuple(packed) == (0.5,0.5,0.5)
+    packed = observations.pack_main_sim_state(
+        main_sim,
+        ("ball_velocity",),
+        box, max_velocity, max_angular_velocity
+    )
+    assert tuple(packed) == (0.25,0.25,0.5)
+
+    packed = observations.pack_main_sim_state(
+        main_sim,
+        ("ball_position","ball_velocity",),
+        box, max_velocity, max_angular_velocity
+    )
+    assert tuple(packed) == (0.5,0.75,0.5,0.25,0.25,0.5)
+
+    packed = observations.pack_main_sim_state(
+        main_sim,
+        ("ball_velocity","ball_position"),
+        box, max_velocity, max_angular_velocity
+    )
+    assert tuple(packed) == (0.25,0.25,0.5,0.5,0.75,0.5)
+
+    packed = observations.pack_main_sim_state(
+        main_sim,
+        ("joint_positions",),
+        box, max_velocity, max_angular_velocity
+    )
+    assert tuple(packed) == (0.5,0.5,0.5,0.5)
+
+    packed = observations.pack_main_sim_state(
+        main_sim,
+        ("racket_cartesian",),
+        box, max_velocity, max_angular_velocity
+    )
+    assert tuple(packed) == (0.,0.,0.,1.0,0.5,0.5,0.5,1.0,0.5,0.5,0.5,1.0)
